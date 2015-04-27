79f4634574ecb3e10eb780abb952a581ed9fdeaf
Hila <hilaisraeli3@gmail.com>
2015-04-28 02:36:57 +0300

    update console mode

--- CatAndMouse/src/main/CatAndMouse.c
+++ CatAndMouse/src/main/CatAndMouse.c
@@ -9,11 +9,25 @@ int main(int argc, char* argv[]) {
 	} 
 	else if (argc == 3){
 		if (!strcmp(argv[1], "-console"))
+		{
 			if ((!strcmp(argv[2], "mouse")) || (!strcmp(argv[2], "cat")))
-				handle_event_console();
+				{
+					handle_event_console();
+				}
+			else
+			{
+				printf("%s","the arguments to the program are not valid\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			printf("%s","the arguments to the program are not valid\n");
+			exit(1);
+		}
 	}
 	else{ //if the argument are not valid to the program - quit
-		printf("%s","the arguments to the program are not valid\n");
+		printf("%s","the number of arguments to the program is not valid\n");
 		exit(1);
 	}
 	return 0;
